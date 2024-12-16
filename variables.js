56b6c131b896d1026f8dc4a98e423c7855ff1310
Tejas Jadhav <163376100+Jadhav-tej@users.noreply.github.com>
2024-12-16 17:54:12 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,37 @@
+// What is javascript?
+
+// JavaScript: JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.\
+// JavaScript is a single-threaded language which means it executes one task at a time.
+// It is an Interpreted language which means it executes the code line by line.
+// The data type of the variable is decided at run-time in JavaScript that’s why it is called dynamically typed.
+
+// Variables and Data Types in JavaScript:
+
+// JavaScript offers three ways to declare variables:
+
+// 1)Var:
+//  A var variable can be redeclared and updated, and it has global or function scope.
+
+    var x=30;
+    console.log(x)  
+    // Output 30
+
+// 2)let:
+// A let variable cannot be redeclared but can be updated, and it has block scope
+
+    let y=12;
+    y = 25; // Allowed
+    console.log(y)
+     // Output 12
+
+// 3)const
+// A const variable cannot be redeclared or updated, and it also has block scope.
+
+    const z=23;
+    // z = 35; // Error: Assignment to constant variable
+    console.log(z)
+     // Output 23
+
+
+    
+   
